fb6efc622dae67ad2a7698b8123f8344f407daf0
luiswebneo <luisfernandolopes0@gmail.com>
2023-10-30 09:29:38 -0300

    Finalizado a parte de templates

--- core/views.py
+++ core/views.py
@@ -2,3 +2,10 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
   template_name = 'index.html'
+
+
+class TesteView(TemplateView):
+  template_name = '404.html'
+
+class TesteView(TemplateView):
+  template_name = '500.html'
